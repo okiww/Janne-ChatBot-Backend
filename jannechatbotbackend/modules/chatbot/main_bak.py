91f3063ba55dc78fc5a3bfc5bdbabe0c251f71a7
okky <okky@99.co>
2019-01-15 10:29:46 +0700

    remove jun code

--- jannechatbotbackend/modules/chatbot/main_bak.py
+++ jannechatbotbackend/modules/chatbot/main_bak.py
@@ -1,10 +1,3 @@
-import re
-import random
-from bottle import response
-from json import dumps
-from common.dictionary.dictionary import reflections
-from common.dictionary.dictionary import psychobabble
- 
 # reflections = {
 #     "am": "are",
 #     "was": "were",
@@ -227,67 +220,3 @@ from common.dictionary.dictionary import psychobabble
 #       "How does that make you feel?",
 #       "How do you feel when you say that?"]]
 # ]
-optionMessage = [
-  {"id": 1, "message": "Cari Property Di Bandung"},
-  {"id": 2, "message": "Saya sedang cari rumah"},
-  {"id": 3, "message": "i'm feeling lucky!"}
-]
-introMessage = [
-  { "id": 1, "message": "Janne the TeenBoT", "type": "message" }, 
-  { "id": 2, "message": "hi!! who r u??!", "type": "message"},
-  { 
-    "id": 3, 
-    "data": optionMessage,
-    "type": "options"
-  },
-]
-
-def reflect(fragment):
-    tokens = fragment.lower().split()
-    for i, token in enumerate(tokens):
-        if token in reflections:
-            tokens[i] = reflections[token]
-    return ' '.join(tokens)
- 
- 
-def analyze(statement):
-    for pattern, responses in psychobabble:
-        match = re.match(pattern, statement.rstrip(".!"))
-        if match:
-            response = random.choice(responses)
-            return response.format(*[reflect(g) for g in match.groups()])
- 
- 
-def main():
-    print "Hello. How are you feeling today?"
-    while True:
-        statement = raw_input("> ")
-        print "Hello. How are you feeling today?"
-        print analyze(statement)
- 
-        if statement == "quit":
-            break
-
-def intro():
-    response.content_type = 'application/json'
-    return dumps(introMessage)
-
-def store(message, type):
-    while True:
-      analyzeMessage = analyze(message)
-      validateMessage = validate(analyzeMessage)
-      
-      response.content_type = 'application/json' 
-      return dumps(validateMessage)
-
-def validate(message):
-  rv = { "id": 1, "message": message }
-  options = [
-    rv,
-    {"id": 2, "data": optionMessage, "type": "options"}
-  ]
-
-  if message == "I dont understand what you said":
-    return options
-  else:
-    return rv
