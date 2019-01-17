171b77c02f515f186c224634eea5fae7222f8151
madekrishna <madekr@live.com>
2019-01-17 15:19:24 +0700

    fixing style code home.py

--- home.py
+++ home.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 import bottle
-from bottle import Bottle, template, route
-from bottle import post, get, put, delete, response, request, run
+from bottle import Bottle, route
+from bottle import response, request, run
 
 import os
-import sys
 import json
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from modules.chatbot.chatbot import intro
 from modules.chatbot.chatbot import store
+
 
 # the decorator
 def enable_cors(fn):
@@ -17,7 +15,8 @@ def enable_cors(fn):
         # set CORS headers
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type,' \
+                                                           ' X-Requested-With, X-CSRF-Token'
 
         if bottle.request.method != 'OPTIONS':
             # actual request; reply with the actual response
@@ -25,12 +24,15 @@ def enable_cors(fn):
 
     return _enable_cors
 
+
 home_app = Bottle()   
+
 
 @route('/')
 @enable_cors
 def index():
     return intro()
+
 
 @route('/post', 'POST')
 @enable_cors
@@ -39,11 +41,11 @@ def home():
     jsondata = json.loads(postdata)
     
     message = jsondata.get('message')
-    typeMessage = jsondata.get('type')
-    return store(message, typeMessage)
+    type_message = jsondata.get('type')
+    return store(message, type_message)
+
 
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
     run(host='localhost', port=8080, debug=True)
-
