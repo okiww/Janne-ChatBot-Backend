7d437570cbaeb9ea8ce0f58714a2d824477ff249
okky <okky@99.co>
2019-01-17 14:45:15 +0700

    enable cors

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import bottle
 from bottle import Bottle, template, route
 from bottle import post, get, put, delete, response, request, run
 
@@ -9,6 +10,21 @@ import json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from modules.chatbot.chatbot import intro
 from modules.chatbot.chatbot import store
+
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
 home_app = Bottle()
 
 
